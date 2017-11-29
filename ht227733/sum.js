0138b505661821b83da09e78c3796bebc0c4b3bd
canhphong <canhphong650@gmail.com>
2017-11-30 06:27:46 +0700

    add folder ht227 and folder xa27

---
+++ ht227733/sum.js
@@ -0,0 +1,37 @@
+var hesos = [ 120, 60, 26, 11, 9, 35, 25, 45, 60, 90, 100];
+var sum = 0;
+var dai;
+var	rong;
+$('#no1').change(function() {
+	dai =  $('#no1').val();
+	tinhTong();
+	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
+});
+
+$('#no2').change(function() {
+	rong =  $('#no2').val();
+	tinhTong();
+	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
+});
+
+function tinhTong() {
+	sum = 0;
+	for (var i = 0; i < $('.game').length; i++) {
+        money = (parseInt(dai) + parseInt(rong))*2+(parseInt(dai) + parseInt(rong))*2*hesos[i]*0.2;
+		if (($('#game'+(i)).is(':checked'))) {
+			sum += money;
+		}
+	}
+}
+
+$('.game').click(function() {
+	var index = $('.game').index(this);
+    money = (parseInt(dai) + parseInt(rong))*2+(parseInt(dai) + parseInt(rong))*2*hesos[index]*0.2;
+	if (!($('#game'+(index)).is(':checked'))) {
+		money = -money;
+	}
+	sum += money;
+ 	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
+});
+
+
