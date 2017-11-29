a25fd8a75c5b58b5ecd5840f6f048c3aba10d680
canhphong <canhphong650@gmail.com>
2017-11-30 06:00:22 +0700

    add fixed tinh toan so theo chu vi

--- chuvi/sum.js
+++ chuvi/sum.js
@@ -1,24 +1,24 @@
 var hesos = [ 120, 60, 26, 11, 9, 35, 25, 45, 60, 90, 100];
 var sum = 0;
 var dai;
-var	rong ;
-var money;
+var	rong;
 $('#no1').change(function() {
 	dai =  $('#no1').val();
 	tinhTong();
-	$('#totalcost').val((sum != 0) ? sum.toFixed(3) : 0);
+	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
 });
 
 $('#no2').change(function() {
 	rong =  $('#no2').val();
 	tinhTong();
-	$('#totalcost').val((sum != 0) ? sum.toFixed(3) : 0);
+	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
 });
 
 function tinhTong() {
 	sum = 0;
 	for (var i = 0; i < $('.game').length; i++) {
-		money = hesos[i]*(2*(dai + rong));
+
+		money = (parseInt(dai) + parseInt(rong))*2*hesos[i];
 		if (($('#game'+(i)).is(':checked'))) {
 			sum += money;
 		}
@@ -27,10 +27,12 @@ function tinhTong() {
 
 $('.game').click(function() {
 	var index = $('.game').index(this);
-	money = hesos[index]*(2*(dai + rong));
+    money = (parseInt(dai) + parseInt(rong))*2*hesos[index];
 	if (!($('#game'+(index)).is(':checked'))) {
 		money = -money;
 	}
 	sum += money;
- 	$('#totalcost').val((sum != 0) ? sum.toFixed(2) : 0);
+ 	$('#totalcost').val((sum != 0) ? Math.ceil(sum) : 0);
 });
+
+
